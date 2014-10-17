0b43473b5c84c451fbdd86abff2344d9c4d38ab8
Ryan Joy <atxryan@gmail.com>
2014-10-17 16:17:01 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,94 @@
+﻿
+/*
+ * GET home page.
+ */
+
+var nconf = require("nconf");
+var DocumentDBClient = require("documentdb").DocumentClient;
+
+nconf.env();
+nconf.file({ file: 'config.json' });
+
+var host = nconf.get("HOST");
+var authKey = nconf.get("AUTH_KEY");
+var databaseId = nconf.get("DATABASE");
+var collectionId = nconf.get("COLLECTION");
+
+var client = new DocumentDBClient(host, { masterKey: authKey });
+
+exports.index = function (req, res) {
+    res.render('index', { title: 'Hello HackTX!' });
+};
+
+exports.createItem = function (req, res) {
+
+    readOrCreateDatabase(function (database) {
+        readOrCreateCollection(database, function (collection) {
+            var item = req.body.item;
+            if (item) {
+                createItem(collection, item, function () { 
+                    res.redirect('/');
+                });
+            } else { 
+                // Throw some error
+            }
+        });
+    });
+
+};
+
+// if the database does not exist, then create it, else return the database object
+var readOrCreateDatabase = function (callback) {
+    
+    client
+        .queryDatabases('SELECT * FROM root r WHERE r.id="' + databaseId + '"')
+        .toArray(function (err, results) {
+            if (err) {
+                // some error occured, rethrow up
+                throw (err);
+            }
+            if (!err && results.length === 0) {
+                // no error occured, but there were no results returned 
+                // indicating no database exists matching the query            
+                client.createDatabase({ id: databaseId }, function (err, createdDatabase) {
+                    callback(createdDatabase);
+                });
+            } else {
+                // we found a database
+                callback(results[0]);
+            }
+        });
+};
+
+// if the collection does not exist for the database provided, create it, else return the collection object
+var readOrCreateCollection = function (database, callback) {
+    
+    client
+        .queryCollections(database._self, 'SELECT * FROM root r WHERE r.id="' + collectionId + '"')
+        .toArray(function (err, results) {
+            if (err) {
+                // some error occured, rethrow up
+                throw (err);
+            }
+            if (!err && results.length === 0) {
+                // no error occured, but there were no results returned 
+                //indicating no collection exists in the provided database matching the query
+                client.createCollection(database._self, { id: collectionId }, function (err, createdCollection) {
+                    callback(createdCollection);
+                });
+            } else {
+                // we found a collection
+                callback(results[0]);
+            }
+        });
+};
+
+var createItem = function (collection, documentDefinition, callback) {
+    client.createDocument(collection._self, documentDefinition, function (err, doc) { 
+        if (err) { 
+            throw (err);
+        }
+
+        callback();
+    });
+};
