e781c720fa53c5cfd368c35198e2c738399b28c7
Ryan Joy <atxryan@gmail.com>
2014-10-21 13:17:04 -0500

    Adding Basic Auth

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ var routes = require('./routes');
 var emails = require('./routes/emails');
 var http = require('http');
 var path = require('path');
+var nconf = require("nconf");
+
+// Create nconf environtment
+nconf
+    .file({ file: 'config.json' })
+    .env();
+var demo_password = nconf.get("DEMO_PASSWORD");
+
+var auth = express.basicAuth(function (username, password, callback) {
+    var result = ((username === 'microsoft') && (password === demo_password));
+    callback(null, result);
+});
+
 
 var app = express();
 
@@ -31,7 +44,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.post('/', routes.createItem);
-app.get('/list-emails', emails.list);
+app.get('/list-emails', auth, emails.list);
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
